9440471df8243c77f4412a97119c5372ac83344a
Richard Wensel <46492607+rwensel@users.noreply.github.com>
2023-04-28 20:43:37 -0400

    updategit ignore, consolidated logging into dbapp

--- FBPageTools/dbapp.py
+++ FBPageTools/dbapp.py
@@ -1,7 +1,36 @@
 import sqlite3
 import time
 
-from .dblogging import log_to_database
+
+# ============================
+# Function: log_to_database
+# ============================
+def log_to_database(database_name, level, message, function):
+    """
+    Log a message to the fb_logging table.
+    Parameters:
+        database_name (str): Path to the application db.
+        level (str): The log level, e.g., 'DEBUG', 'INFO', 'WARNING', 'ERROR', or 'CRITICAL'.
+        message (str): The log message.
+        function (str): The name of the function where the log entry was created.
+
+    Returns:
+        None
+    """
+    # Connect to the SQLite database
+    conn = sqlite3.connect(database_name)
+    c = conn.cursor()
+
+    # Insert the log entry into the fb_logging table
+    try:
+        c.execute('INSERT INTO fb_logging (timestamp, level, message, function) VALUES (?, ?, ?, ?)',
+                  (datetime.now(), level, message, function))
+        conn.commit()
+    except Exception as e:
+        print(f'Error logging to database: {str(e)}')
+
+    # Close the connection
+    conn.close()
 
 
 # ============================
